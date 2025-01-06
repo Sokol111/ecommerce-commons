ec39bffbcac96109f642945f999fd956abbc31b4
Sokol111 <igorsokol111@gmail.com>
2025-01-06 19:33:40 +0100

    Changed

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,13 +9,13 @@ func LoadConfig[T any](configFile string) *T {
 	v := viper.New()
 	v.SetConfigFile(configFile)
 	v.AddConfigPath(".")
-	viper.AutomaticEnv()
-	err := viper.ReadInConfig()
+	v.AutomaticEnv()
+	err := v.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("failed to read config file [%s]: %s", configFile, err))
 	}
 	var conf T
-	err = viper.Unmarshal(conf)
+	err = v.Unmarshal(conf)
 	if err != nil {
 		panic(fmt.Errorf("failed to unmarshal config file [%s] to type [%T]: %s", configFile, conf, err))
 	}

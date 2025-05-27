94e57295a865f1051020d63cbb2f6653cdf877ad
Sokol111 <igorsokol111@gmail.com>
2025-05-27 15:57:40 +0200

    Refactored

--- pkg/module/kafka.go
+++ pkg/module/kafka.go
@@ -1,14 +1,14 @@
 package module
 
 import (
-	"github.com/Sokol111/ecommerce-commons/pkg/kafka"
+	"github.com/Sokol111/ecommerce-commons/pkg/kafka/config"
 	"github.com/Sokol111/ecommerce-commons/pkg/kafka/outbox"
 	"github.com/Sokol111/ecommerce-commons/pkg/kafka/producer"
 	"go.uber.org/fx"
 )
 
 var KafkaWritingModule = fx.Options(
-	kafka.KafkaConfigModule,
+	config.KafkaConfigModule,
 	producer.ProducerModule,
 	outbox.OutboxModule,
 )
